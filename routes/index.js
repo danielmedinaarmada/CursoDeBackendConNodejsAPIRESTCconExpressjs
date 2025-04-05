5ff8a5f42c839df72ec9a03049374ee1d32801a1
Daniel Medina <daniel@medinaarmada.com>
2025-04-05 10:40:12 -0400

    Ejemplo de versionamiento de la APP

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 import defaultRouter from './default.router.js';
+import defaultV2Router from './default.V2.router.js';
 import inicioRouter from './inicio.router.js';
 import nuevaRutaRouter from './nueva-ruta.router.js';
 import productRouter from './products.router.js';
@@ -6,12 +7,13 @@ import usersRouter from './users.router.js';
 import categoriesRouter from './categories.router.js';
 
 function routerApi(app) {
-  app.use('/', defaultRouter);
-  app.use('/nueva-ruta', nuevaRutaRouter);
-  app.use('/inicio', inicioRouter);
-  app.use('/products', productRouter);
-  app.use('/users', usersRouter);
-  app.use('/categories', categoriesRouter);
+  app.use('/api/', defaultRouter);
+  app.use('/api/v2', defaultV2Router);
+  app.use('/api/nueva-ruta', nuevaRutaRouter);
+  app.use('/api/inicio', inicioRouter);
+  app.use('/api/products', productRouter);
+  app.use('/api/users', usersRouter);
+  app.use('/api/categories', categoriesRouter);
 }
 
 export default routerApi;
