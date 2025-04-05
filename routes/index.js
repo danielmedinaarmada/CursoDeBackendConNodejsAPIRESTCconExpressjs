8b1b006bd51317c19b18227e14c63fe4b18a12d8
Daniel Medina <daniel@medinaarmada.com>
2025-04-05 10:58:10 -0400

    otra forma de versionar

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+import express from 'express';
+
 import defaultRouter from './default.router.js';
 import defaultV2Router from './default.V2.router.js';
 import inicioRouter from './inicio.router.js';
@@ -7,13 +9,19 @@ import usersRouter from './users.router.js';
 import categoriesRouter from './categories.router.js';
 
 function routerApi(app) {
-  app.use('/api/', defaultRouter);
-  app.use('/api/v2', defaultV2Router);
-  app.use('/api/nueva-ruta', nuevaRutaRouter);
-  app.use('/api/inicio', inicioRouter);
-  app.use('/api/products', productRouter);
-  app.use('/api/users', usersRouter);
-  app.use('/api/categories', categoriesRouter);
+  const router = express.Router();
+  app.use("/api/v1", router);
+  router.use('/', defaultRouter);
+  router.use('/nueva-ruta', nuevaRutaRouter);
+  router.use('/inicio', inicioRouter);
+  router.use('/products', productRouter);
+  router.use('/users', usersRouter);
+  router.use('/categories', categoriesRouter);
+
+  const router2 = express.Router();
+  app.use("/api/v2", router2);
+  router2.use('/', defaultV2Router);
+
 }
 
 export default routerApi;
