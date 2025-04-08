e4c90a7566e72d51d01cedb9f33a9cba46d8ffd0
Daniel Medina <daniel@medinaarmada.com>
2025-04-08 18:54:01 -0400

    white list para conexiones externas

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 import express from 'express';
 import cors from 'cors';
 import routerApi from './routes/index.js';
-import { logErrors, errorHandler, boomErrorHandler } from './middlewares/error.handler.js';
+import {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} from './middlewares/error.handler.js';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(cors());
+
+const options = {
+  origin: (origin, callback) => {
+    if(whitelist.includes(origin)) {
+      callback("", true)
+    } else {
+      callback(new Error ("No permitido"))
+    }
+  },
+};
+const whitelist = [
+
+  'https://myapp.com',
+];
+
+app.use(cors(options));
 
 routerApi(app);
 
