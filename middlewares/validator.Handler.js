25007d6fc489b725d4dfbe222eb5e9e47fb42a31
Daniel Medina <daniel@medinaarmada.com>
2025-04-08 18:24:18 -0400

    para ver todos los errores de joi en un solo menssage

--- middlewares/validator.Handler.js
+++ middlewares/validator.Handler.js
@@ -3,7 +3,9 @@ import boom from '@hapi/boom';
 function validatorHandler(schema, property) {
   return (req, res, next) => {
     const data = req[property];
-    const options = { abortEarly: false };
+    const options = {
+      abortEarly: false,
+    };
     //la info viene de varios lugares.
     //post -> req.body;
     // get -> req.params;
